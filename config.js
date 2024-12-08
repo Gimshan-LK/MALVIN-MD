e215a63bc287d33a22a9c288b5fd014f2276d47e
Gimshan-LK <dilishagimshan99@gmail.com>
2024-12-08 10:15:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 ///=================When editing, do not accidentally delete anything. !!MAIN-CONFIG=================================
-SESSION_ID: process.env.SESSION_ID || "yBp0EDbI#rbwRkL_W2ANQDWayoPzpqrKpnY25RXcG3UWq7XVMFt8",  // ADD Your Session Id 
+SESSION_ID: process.env.SESSION_ID || "2JlwmATD#zLegNqDof9o5a9R08pD-_eaz35j3zTtGQaV_BNEuhOY",  // ADD Your Session Id 
 MODE: process.env.MODE || "public",  // Add Your Bot Mode (groups/privet/public)
 PREFIX: process.env.PREFIX || ".",   // Add Your Custom Prefix 
 OWNER_REACT: process.env.OWNER_REACT || "true", //add true or false
